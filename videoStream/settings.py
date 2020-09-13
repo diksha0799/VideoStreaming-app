b07e37cd343e9b5c60d2600880a14a794fc77b18
diksha <65006493+diksha0799@users.noreply.github.com>
2020-09-14 00:52:35 +0530

    Update settings.py

--- videoStream/settings.py
+++ videoStream/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoise.Middleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +117,5 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+# https://docs.djangoproject.com/en/3.0/howto/static-files
 STATIC_URL = '/static/'
