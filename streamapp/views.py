08ea549397c0d2d8ccfb262421d0271d05a84445
diksha <diksha.india712@gmail.com>
2020-09-13 21:02:29 +0530

    video_streaming

---
+++ streamapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http.response import StreamingHttpResponse
+from streamapp.camera import VideoCamera, IPWebCam, LiveWebCam
+# Create your views here.
+
+
+def index(request):
+	return render(request, 'streamapp/home.html')
+
+
+def gen(camera):
+	while True:
+		frame = camera.get_frame()
+		yield (b'--frame\r\n'
+				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+def video_feed(request):
+	return StreamingHttpResponse(gen(VideoCamera()),
+					content_type='multipart/x-mixed-replace; boundary=frame')
+
+
+def webcam_feed(request):
+	return StreamingHttpResponse(gen(IPWebCam()),
+					content_type='multipart/x-mixed-replace; boundary=frame')
+
+
+
+def livecam_feed(request):
+	return StreamingHttpResponse(gen(LiveWebCam()),
+					content_type='multipart/x-mixed-replace; boundary=frame')
